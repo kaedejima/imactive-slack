c708f833a22f204ffae58e734c1be8669f6d8089
“kaedejima” <frogtoadie@gmail.com>
2020-11-04 12:40:31 +0900

    week06 change status

--- 06.py
+++ 06.py
@@ -1,3 +1,4 @@
+# RUN FROM TERMINAL
 # cd Documents/Waseda3rdYear/ProjectResearchB/slack_api
 # python3 06.py
 
@@ -5,6 +6,7 @@ import cv2
 from datetime import datetime
 import dlib
 from imutils import face_utils
+import imutils
 
 import subprocess
 import os
@@ -12,6 +14,7 @@ from dotenv import load_dotenv
 # from slackclient import SlackClient
 import requests
 from datetime import date
+import json
 
 from slack import WebClient
 
@@ -19,7 +22,9 @@ load_dotenv()
 
 # slack_token = os.environ['SLACK_TOKEN']
 # client = SlackClient(slack_token)
-client = WebClient(token=os.environ['SLACK_TOKEN'])
+# client = WebClient(token=os.environ['SLACK_USER_TOKEN'])
+slack_oauth_token = os.environ['SLACK_OAUTH_TOKEN']
+user_id = os.environ['USER_ID']
 
 face_detector = dlib.get_frontal_face_detector()
 predictor_path = 'shape_predictor_68_face_landmarks.dat'
@@ -40,21 +45,32 @@ class SlackDriver:
 
         # print("return ", r.json())
 
-    def change_status(self, status):
-        params = {"token": self._token, "channel": channel, "text": message}
+    def change_status(self, text, emoji):
+        params = {
+            "token": self._token,
+            # "user": user_id,
+            "profile": json.dumps({
+                "status_text": text,
+                "status_emoji": emoji
+            })
+        }
+        # print(params)
 
-        r = requests.post('https://slack.com/api/users.profile.set',
-                          headers=self._headers, params=params)
+        r = requests.post(
+            'https://slack.com/api/users.profile.set', params=params)
+        # print(r.text)
 
 
-def cpture_save():
+def cpture():
     cap = cv2.VideoCapture(0)
+    # cap.set(4, 320)
     n = 0
     no_face_for = 0
     hello_for = 0
     while True:
         # get 1frame
         ret, frame = cap.read()
+        frame = imutils.resize(frame, width=320)
         # if no frame then close
         if (not ret):
             break
@@ -64,23 +80,21 @@ def cpture_save():
 
         dets = face_detector(frame, 1)
         det_str = str(dets)
-        print(str(dets))
+        # print(str(dets))
         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(date)
+        # print(date)
         if (det_str == 'rectangles[]'):
             no_face_for += 1
             if no_face_for == 10:
                 hello_for = 0
                 print('no face')
-                date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                slack.send_message(
-                    "You are offline... [" + date + "]", "#imactive-response")
+                detected_inactive(date)
         else:
             hello_for += 1
             if hello_for == 10:
                 no_face_for = 0
-                slack.send_message(
-                    "Hello, you are here! [" + date + "]", "#imactive-response")
+                print('hello face')
+                detected_active(date)
 
         # show window
         cv2.imshow("Frame", frame)
@@ -91,11 +105,24 @@ def cpture_save():
         n += 1
 
     cap.release()
+    slack.change_status('', '')
     cv2.destroyAllWindows()
 
 
+def detected_inactive(date):
+    slack.send_message(
+        "You are offline... [" + date + "]", "#imactive-response")
+    slack.change_status('I am offline...', ':hear_no_evil:')
+
+
+def detected_active(date):
+    slack.send_message(
+        "Hello, you are here! [" + date + "]", "#imactive-response")
+    slack.change_status('Hi, I am available!', ':thumbsup:')
+
+
 if __name__ == '__main__':
-    token = os.environ['SLACK_TOKEN']  # TODO your token.
+    token = slack_oauth_token
     slack = SlackDriver(token)
 
-    cpture_save()
+    cpture()
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-# cd Documents/Waseda3rdYear/ProjectResearchB/slack_api
-# python3 test.py
-
 import cv2
 from datetime import datetime
 import dlib
@@ -9,83 +6,43 @@ from imutils import face_utils
 import subprocess
 import os
 from dotenv import load_dotenv
-from slackclient import SlackClient
+
 import requests
 from datetime import date
+import json
 
+from slack import WebClient
 
 load_dotenv()
 
-slack_token = os.environ['SLACK_TOKEN']
-client = SlackClient(slack_token)
-
-face_detector = dlib.get_frontal_face_detector()
-predictor_path = 'shape_predictor_68_face_landmarks.dat'
-face_predictor = dlib.shape_predictor(predictor_path)
+# client = WebClient(token=os.environ['SLACK_OAUTH_TOKEN'])
+slack_oauth_token = os.environ['SLACK_OAUTH_TOKEN']
+user_id = os.environ['USER_ID']
 
 
 class SlackDriver:
 
     def __init__(self, _token):
-        self._token = _token  # api_token
+        self._token = _token
         self._headers = {'Content-Type': 'application/json'}
 
-    def send_message(self, message, channel):
-        params = {"token": self._token, "channel": channel, "text": message}
+    def change_status(self, text, emoji):
+        data = {
+            "token": os.environ['SLACK_OAUTH_TOKEN'],
+            # "user": user_id,
+            "profile": json.dumps({
+                "status_text": text,
+                "status_emoji": emoji
+            })
+        }
+        print(data)
 
-        r = requests.post('https://slack.com/api/chat.postMessage',
-                          headers=self._headers,
-                          params=params)
-
-        # print("return ", r.json())
-
-
-def cpture_save(cycle):
-    cap = cv2.VideoCapture(0)
-    n = 0
-    while True:
-        # get 1frame
-        ret, frame = cap.read()
-        # if no frame then close
-        if not ret:
-            break
-        if n < 10:
-            n += 1
-
-        # if n % cycle == 0:
-        #     date = datetime.now().strftime('%Y-%m-%d%H:%M:%S')
-        #     path = './img/' + date + '.jpg'
-        #     print(path)
-        #     cv2.imwrite(path, frame)
-        #     n += 1
-
-        dets = face_detector(frame, 1)
-        slack.send_message("Hello World! from python" +
-                           str(today), "#imactive-response")
-        print(str(dets))
-        det_str = str(dets)
-        if (det_str == 'rectangles[]'):
-            print('no face')
-            # slack.send_message("Hello World! from python" +
-            #                    str(today), "#imactive-response")
-
-            # show window
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1)
-        # EscKey closes the program
-        if key == 27:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+        r = requests.post(
+            'https://slack.com/api/users.profile.set', params=data)
+        print(r.text)
 
 
 if __name__ == '__main__':
-    token = os.environ['SLACK_TOKEN']  # TODO your token.
+    token = slack_oauth_token
     slack = SlackDriver(token)
-    today = date.today()
-
-    cpture_save(2)
-
-    # slack.send_message("Hello World! from python" +
-    #                    str(today), "#imactive-response")
+    slack.change_status("hello from 'test.py'", ':smile:')
